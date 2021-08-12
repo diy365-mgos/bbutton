a3027631f60efb5363ac97f050b226bc88082d8b
fbalestrini <gaojing@hotmail.it>
2021-08-12 16:03:11 +0200

    Updated

--- src/mgos_bbutton.c
+++ src/mgos_bbutton.c
@@ -82,7 +82,7 @@ bool mgos_bbutton_get_cfg(mgos_bbutton_t button, struct mgos_bbutton_cfg *cfg) {
 }
 
 static void mg_bbutton_poll_cb(void *arg) {
-  mgos_bthing_update_states(MGOS_BBUTTON_TYPE);
+  mg_bthing_update_states(MGOS_BBUTTON_TYPE, false);
   (void) arg;
 }
 
